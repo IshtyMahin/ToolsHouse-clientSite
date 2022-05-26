7a4e75d301afd6551e6773f48fff6872b36eb8e5
Ishtiaq uddin <ishty119@gmail.com>
2022-05-26 15:58:23 +0600

    heroku update

--- src/Pages/Dashboard/CheckoutFrom.js
+++ src/Pages/Dashboard/CheckoutFrom.js
@@ -1,118 +1,111 @@
-
-import React, { useEffect, useState } from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 const CheckoutForm = ({ item }) => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const [cardError, setCardError] = useState('');
-    const [success, setSuccess] = useState('');
-    
-    const [clientSecret, setClientSecret] = useState('');
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
 
-    const { _id, price, productName, customerName} = item;
+  const [clientSecret, setClientSecret] = useState("");
 
-    useEffect(() => {
-        fetch('http://localhost:5000/create-payment-intent', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-            },
-            body: JSON.stringify({ price })
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if (data?.clientSecret) {
-                    setClientSecret(data.clientSecret);
-                }
-            });
+  const { _id, price, productName, customerName } = item;
 
-    }, [price])
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        if (!stripe || !elements) {
-            return;
+  useEffect(() => {
+    fetch("https://young-wave-22909.herokuapp.comcreate-payment-intent", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify({ price }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data?.clientSecret) {
+          setClientSecret(data.clientSecret);
         }
+      });
+  }, [price]);
 
-        const card = elements.getElement(CardElement);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        if (card === null) {
-            return;
-        }
-
-        const { error, paymentMethod } = await stripe.createPaymentMethod({
-            type: 'card',
-            card
-        });
-
-        setCardError(error?.message || '')
-        setSuccess('');
-      
-        // confirm card payment
-        const { paymentIntent, error:intentError } = await stripe.confirmCardPayment(
-            clientSecret,
-            {
-                payment_method: {
-                    card: card,
-                    billing_details: {
-                        name: customerName,
-                        product: productName
-                    },
-                },
-            },
-        );
-
-        if (intentError) {
-            console.log(intentError)
-            setCardError(intentError?.message);
-        }
-        else {
-            setCardError('');   
-            console.log(paymentIntent);
-            setSuccess('Congrats! Your payment is completed.')
-            
-          } 
-
-        
+    if (!stripe || !elements) {
+      return;
     }
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <CardElement
-                    options={{
-                        style: {
-                            base: {
-                                fontSize: '16px',
-                                color: '#424770',
-                                '::placeholder': {
-                                    color: '#aab7c4',
-                                },
-                            },
-                            invalid: {
-                                color: '#9e2146',
-                            },
-                        },
-                    }}
-                />
-                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret || success}>
-                    Pay
-                </button>
-            </form>
-            {
-                cardError && <p className='text-red-500'>{cardError}</p>
-            }
-            {
-                success && <div className='text-green-500'>
-                    <p>{success}  </p>
-                    
-                </div>
-            }
-        </>
-    );
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    setCardError(error?.message || "");
+    setSuccess("");
+
+    // confirm card payment
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: customerName,
+            product: productName,
+          },
+        },
+      });
+
+    if (intentError) {
+      console.log(intentError);
+      setCardError(intentError?.message);
+    } else {
+      setCardError("");
+      console.log(paymentIntent);
+      setSuccess("Congrats! Your payment is completed.");
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-success btn-sm mt-4"
+          type="submit"
+          disabled={!stripe || !clientSecret || success}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className="text-red-500">{cardError}</p>}
+      {success && (
+        <div className="text-green-500">
+          <p>{success} </p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CheckoutForm;
